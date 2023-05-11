e17db62c52d55b7e4b648103ee02448167b08f58
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-11 16:24:18 +0200

    Add In.java that can get char from user

---
+++ src/In.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class In
+{
+    static char nextChar()
+    {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next().charAt(0);
+    }
+}
