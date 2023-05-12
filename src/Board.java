57683040ef6177155cca4f128edbb0b19a58f98c
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-12 12:31:41 +0200

    Add printing letters row in Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,17 @@
+import static java.lang.System.out;
+
 public class Board
 {
     static Cell[][] xyAxle = new Cell[10][10];
     static void print()
     {
+        out.print("  ");
+        for (int i = 0; i < 10; i++) {
+            out.print(Convert.numbToLetter(i));
+            out.print("    ");
+        }
+        for (int i = 0; i < 11; i++) {
 
-    }
-    static void clear()
-    {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        }
     }
 }
