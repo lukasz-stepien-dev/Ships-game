924c3e093fc67181b278e9eb3a72a57afd9a0c7f
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-12 12:21:19 +0200

    Make conversion from number to letter

--- src/Convert.java
+++ src/Convert.java
@@ -3,12 +3,21 @@ import java.util.Map;
 
 public class Convert
 {
-    static Integer lettersToNumbs(char letter)
+    static int lettersToNumbs(char letter)
     {
         Map<Character, Integer> values = new LinkedHashMap<>();
         for (int i = 65; i < 75; i++) {
             values.put((char) i, i-65);
         }
         return values.get(letter);
+    }
+    static String numbsToLetters(int numb)
+    {
+        Map<Integer, Character> values = new LinkedHashMap<>();
+        for (int i = 65; i < 75; i++)
+        {
+            values.put(i-65, (char) i);
+        }
+        return Character.toString(values.get(numb));
     }
 }
