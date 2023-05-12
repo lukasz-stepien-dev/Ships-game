32565df7a71816b1a5ee561da31d834e5e8dc7db
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-12 12:53:47 +0200

    Add to Board.java: setEmptyCells and printing cells

--- src/Board.java
+++ src/Board.java
@@ -2,16 +2,32 @@ import static java.lang.System.out;
 
 public class Board
 {
-    static Cell[][] xyAxle = new Cell[10][10];
+    static Cell[][] cells = new Cell[10][10];
+    static void setEmptyCell()
+    {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                cells[i][j] = new Cell(States.empty);
+            }
+        }
+    }
     static void print()
     {
-        out.print("  ");
+        out.print("   ");
         for (int i = 0; i < 10; i++) {
             out.print(Convert.numbToLetter(i));
-            out.print("    ");
+            out.print("  ");
         }
-        for (int i = 0; i < 11; i++) {
-
+        out.print("\n");
+        for (int i = 0; i < 10; i++)
+        {
+            out.print(i);
+            out.print(" ");
+            for (int j = 0; j < 10; j++)
+            {
+                cells[i][j].print();
+            }
+            out.print("\n");
         }
     }
 }
