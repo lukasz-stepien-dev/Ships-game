c3d072df8b46feaa0b8aa2baf1a2715e2efba21b
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-12 12:31:15 +0200

    Add capability inputting int in In.java

--- src/In.java
+++ src/In.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class In
 {
+    private static Scanner scanner = new Scanner(System.in);
     static char nextChar()
     {
-        Scanner scanner = new Scanner(System.in);
         return scanner.next().charAt(0);
+    }
+    static int nextInt()
+    {
+        return scanner.nextInt();
     }
 }
